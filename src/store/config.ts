be94953fed3f012ed984a0cf3c0070dcc4819a2a
NJneeraj <niju7489@gmail.com>
2025-05-12 16:07:02 +0530

    config setup

--- src/store/config.ts
+++ src/store/config.ts
@@ -1,6 +1,6 @@
-const enrironment = import.meta.env.DEV;
+const isDev = import.meta.env.DEV;
 
-export const BASE_URL = enrironment
+export const BASE_URL = isDev
   ? "http://localhost:3000/v1/admin"
   : "https://edesk.esaral.com/v1/admin";
 
